855db18320917a3145d105b252233c7851b6cc2b
Black-Hole <158blackhole@gmail.com>
2020-12-11 20:38:11 +0800

    refactor(server): optimize parameter verification code

--- src/v1/controller/agora/token/RTC.ts
+++ src/v1/controller/agora/token/RTC.ts
@@ -1,24 +1,40 @@
 import { Next, Request, Response } from "restify";
 import { RtcRole, RtcTokenBuilder } from "agora-access-token";
-import { HTTPValidationRules } from "../../../types/Server";
 import { Agora, Status } from "../../../../Constants";
 
 export const generateRTC = async (req: Request, res: Response, next: Next): Promise<void> => {
     const { channelName, uid } = req.body as GenerateRTCBody;
 
-    const token = RtcTokenBuilder.buildTokenWithUid(Agora.APP_ID, Agora.APP_CERTIFICATE, channelName, uid, RtcRole.PUBLISHER, 0);
+    const token = RtcTokenBuilder.buildTokenWithUid(
+        Agora.APP_ID,
+        Agora.APP_CERTIFICATE,
+        channelName,
+        uid,
+        RtcRole.PUBLISHER,
+        0,
+    );
 
     res.send({
         status: Status.Success,
         data: {
             token,
-        }
-    })
+        },
+    });
     next();
 };
 
-export const generateRTCValidationRules: HTTPValidationRules = {
-    body: ["channelName", "uid"],
+export const generateRTCValidationRules = {
+    body: {
+        type: "object",
+        properties: {
+            channelName: {
+                type: "string",
+            },
+            uid: {
+                type: "string",
+            },
+        },
+    },
 };
 
 type GenerateRTCBody = {
