74f80a2960ee56eec77d12d43773266bf76d533e
Black-Hole <158blackhole@gmail.com>
2020-12-11 14:28:10 +0800

    feat(agora): support generate agora token

--- src/v1/Routes.ts
+++ src/v1/Routes.ts
@@ -1,7 +1,8 @@
 import { socketLogin } from "./sockets/login/Routes";
 import { httpLogin } from "./controller/login/Router";
+import { httpAgora } from "./controller/agora/Router";
 import { IORoutes, RestifyRoutes } from "./types/Server";
 
-export const httpRoutes: Readonly<Readonly<RestifyRoutes[]>[]> = Object.freeze([httpLogin]);
+export const httpRoutes: Readonly<Readonly<RestifyRoutes[]>[]> = Object.freeze([httpLogin, httpAgora]);
 
 export const socketRoutes: Readonly<Readonly<IORoutes[]>[]> = Object.freeze([socketLogin]);
