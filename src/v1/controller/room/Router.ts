931763c1347ad95335f726b69b3c5add05e9b0bb
Black-Hole <158blackhole@gmail.com>
2020-12-23 14:32:04 +0800

    refactor(room): create room split into normal creation and schedule room (#47)
    [ci skip]

--- src/v1/controller/room/Router.ts
+++ src/v1/controller/room/Router.ts
@@ -1,5 +1,6 @@
 import { FastifyRoutes } from "../../types/Server";
 import { create, createSchemaType } from "./create";
+import { schedule, scheduleSchemaType } from "./schedule";
 import { list, listSchemaType } from "./list";
 
 export const httpRoom: Readonly<FastifyRoutes[]> = Object.freeze([
@@ -9,6 +10,13 @@ export const httpRoom: Readonly<FastifyRoutes[]> = Object.freeze([
         handler: create,
         auth: true,
         schema: createSchemaType,
+    }),
+    Object.freeze({
+        method: "post",
+        path: "room/schedule",
+        handler: schedule,
+        auth: true,
+        schema: scheduleSchemaType,
     }),
     Object.freeze({
         method: "get",
