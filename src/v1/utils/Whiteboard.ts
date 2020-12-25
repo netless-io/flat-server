8f0d455e914eefc26a1718068f2db9e0e5f82566
Black-Hole <158blackhole@gmail.com>
2020-12-25 11:44:43 +0800

    refactor(whiteboard-token): generate sdk and room token on the server side (#52)

---
+++ src/v1/utils/Whiteboard.ts
@@ -0,0 +1,39 @@
+import { ax } from "./Axios";
+import { shuntCreateRoomURL } from "./WhiteboardURL";
+import { createWhiteboardSDKToken } from "../../utils/NetlessToken";
+
+const makeFetch = async <T>(url: string, body: unknown = undefined): Promise<T> => {
+    const response = await ax.post<T>(url, body, {
+        headers: {
+            token: createWhiteboardSDKToken(),
+            // TODO region: 'cn-hz',
+        },
+    });
+    return response.data;
+};
+
+/**
+ * whiteboard create room api
+ * @param {string} name - room name (max length: 2048)
+ * @param {number} limit - (default: 0 = no limit)
+ * @return {string} whiteboard room uuid, not room model's room_uuid
+ */
+export const whiteboardCreateRoom = async (name: string, limit = 0): Promise<string> => {
+    const { uuid } = await makeFetch<Room>(shuntCreateRoomURL, {
+        name,
+        isRecord: true,
+        limit,
+    });
+
+    return uuid;
+};
+
+type Room = {
+    uuid: string;
+    name: string;
+    teamUUID: string;
+    isRecord: boolean;
+    isBan: boolean;
+    limit: number;
+    createdAt: string;
+};
