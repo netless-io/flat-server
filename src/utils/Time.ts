180ece06e08e17efac0ca2ed434758952313c1f7
Black-Hole <158blackhole@gmail.com>
2020-12-29 17:29:22 +0800

    fix(db): time zone error (#92)

--- src/utils/Time.ts
+++
@@ -1,9 +0,0 @@
-import { toDate } from "date-fns/fp";
-import { DateUtils } from "typeorm/util/DateUtils";
-
-export const UTCDate = (time: string | Date | number = new Date()): Date => {
-    if (typeof time === "number") {
-        return DateUtils.mixedDateToDate(toDate(time), true);
-    }
-    return DateUtils.mixedDateToDate(time, true);
-};
