1da6e678b3c7b098d54e0004eedd8f5be5d3100f
Black-Hole <158blackhole@gmail.com>
2020-12-31 15:47:33 +0800

    refactor(api): merge join api (#98)

--- src/v1/controller/room/join/Periodic.ts
+++ src/v1/controller/room/join/Periodic.ts
@@ -1,5 +1,3 @@
-import { FastifyReply } from "fastify";
-import { FastifySchema, PatchRequest } from "../../../types/Server";
 import { getConnection, getRepository } from "typeorm";
 import { Status } from "../../../../Constants";
 import { RoomModel } from "../../../model/room/Room";
@@ -9,126 +7,92 @@ import { RoomPeriodicConfigModel } from "../../../model/room/RoomPeriodicConfig"
 import { RoomUserModel } from "../../../model/room/RoomUser";
 import cryptoRandomString from "crypto-random-string";
 import { RoomPeriodicUserModel } from "../../../model/room/RoomPeriodicUser";
+import { Result } from "./Type";
 
-export const joinPeriodic = async (
-    req: PatchRequest<{
-        Body: JoinPeriodicBody;
-    }>,
-    reply: FastifyReply,
-): Promise<void> => {
-    const { periodicUUID } = req.body;
-    const { userUUID } = req.user;
+export const joinPeriodic = async (periodicUUID: string, userUUID: string): Promise<Result> => {
+    const roomPeriodicConfig = await getRepository(RoomPeriodicConfigModel).findOne({
+        select: ["periodic_status"],
+        where: {
+            periodic_uuid: periodicUUID,
+            is_delete: false,
+        },
+    });
 
-    try {
-        const roomPeriodicConfig = await getRepository(RoomPeriodicConfigModel).findOne({
-            select: ["periodic_status"],
-            where: {
-                periodic_uuid: periodicUUID,
-                is_delete: false,
-            },
-        });
+    if (roomPeriodicConfig === undefined) {
+        return {
+            status: Status.Failed,
+            message: "Periodic room not found",
+        };
+    }
 
-        if (roomPeriodicConfig === undefined) {
-            return reply.send({
-                status: Status.Failed,
-                message: "Periodic room not found",
-            });
-        }
+    if (roomPeriodicConfig.periodic_status === RoomStatus.Stopped) {
+        return {
+            status: Status.Failed,
+            message: "Periodic has been ended",
+        };
+    }
 
-        if (roomPeriodicConfig.periodic_status === RoomStatus.Stopped) {
-            return reply.send({
-                status: Status.Failed,
-                message: "Periodic has been ended",
-            });
-        }
-
-        const roomInfo = await getRepository(RoomModel)
-            .createQueryBuilder()
-            .select(["room_uuid", "whiteboard_room_uuid", "owner_uuid", "room_status"])
-            .where(
-                `periodic_uuid = :periodicUUID
+    const roomInfo = await getRepository(RoomModel)
+        .createQueryBuilder()
+        .select(["room_uuid", "whiteboard_room_uuid", "owner_uuid", "room_status"])
+        .where(
+            `periodic_uuid = :periodicUUID
                 AND room_status IN (:...roomStatus)
                 AND is_delete = false`,
-                {
-                    periodicUUID,
-                    roomStatus: [RoomStatus.Pending, RoomStatus.Running],
-                },
-            )
-            .getRawOne();
-
-        // will arrive here in extreme cases, notify user to retry
-        if (roomInfo === undefined) {
-            return reply.send({
-                status: Status.Failed,
-                message: "Room has ended or been deleted",
-            });
-        }
-
-        await getConnection().transaction(async t => {
-            const commands: Promise<unknown>[] = [];
-
-            commands.push(
-                t
-                    .createQueryBuilder()
-                    .insert()
-                    .into(RoomUserModel)
-                    .orIgnore()
-                    .values({
-                        room_uuid: roomInfo.room_uuid,
-                        user_uuid: userUUID,
-                        user_int_uuid: cryptoRandomString({ length: 10, type: "numeric" }),
-                    })
-                    .execute(),
-            );
-
-            commands.push(
-                t
-                    .createQueryBuilder()
-                    .insert()
-                    .into(RoomPeriodicUserModel)
-                    .orIgnore()
-                    .values({
-                        periodic_uuid: periodicUUID,
-                        user_uuid: userUUID,
-                    })
-                    .execute(),
-            );
-
-            return await Promise.all(commands);
-        });
-
-        return reply.send({
-            status: Status.Success,
-            data: {
-                roomUUID: roomInfo.room_uuid,
-                whiteboardRoomToken: createWhiteboardRoomToken(roomInfo.whiteboard_room_uuid),
-                whiteboardRoomUUID: roomInfo.whiteboard_room_uuid,
+            {
+                periodicUUID,
+                roomStatus: [RoomStatus.Pending, RoomStatus.Running],
             },
-        });
-    } catch (e) {
-        console.error(e);
-        return reply.send({
+        )
+        .getRawOne();
+
+    // will arrive here in extreme cases, notify user to retry
+    if (roomInfo === undefined) {
+        return {
             status: Status.Failed,
-            message: "Join room failed",
-        });
+            message: "Room has ended or been deleted",
+        };
     }
-};
 
-interface JoinPeriodicBody {
-    periodicUUID: string;
-}
+    await getConnection().transaction(async t => {
+        const commands: Promise<unknown>[] = [];
 
-export const joinPeriodicSchemaType: FastifySchema<{
-    body: JoinPeriodicBody;
-}> = {
-    body: {
-        type: "object",
-        required: ["periodicUUID"],
-        properties: {
-            periodicUUID: {
-                type: "string",
-                maxLength: 40,
-            },
+        commands.push(
+            t
+                .createQueryBuilder()
+                .insert()
+                .into(RoomUserModel)
+                .orIgnore()
+                .values({
+                    room_uuid: roomInfo.room_uuid,
+                    user_uuid: userUUID,
+                    user_int_uuid: cryptoRandomString({ length: 10, type: "numeric" }),
+                })
+                .execute(),
+        );
+
+        commands.push(
+            t
+                .createQueryBuilder()
+                .insert()
+                .into(RoomPeriodicUserModel)
+                .orIgnore()
+                .values({
+                    periodic_uuid: periodicUUID,
+                    user_uuid: userUUID,
+                })
+                .execute(),
+        );
+
+        return await Promise.all(commands);
+    });
+
+    return {
+        status: Status.Success,
+        data: {
+            roomUUID: roomInfo.room_uuid,
+            whiteboardRoomToken: createWhiteboardRoomToken(roomInfo.whiteboard_room_uuid),
+            whiteboardRoomUUID: roomInfo.whiteboard_room_uuid,
         },
-    },
+    };
 };
---
+++ src/v1/controller/room/join/index.ts
@@ -0,0 +1,61 @@
+import { FastifyReply } from "fastify";
+import { FastifySchema, PatchRequest } from "../../../types/Server";
+import { getRepository } from "typeorm";
+import { Status } from "../../../../Constants";
+import { joinOrdinary } from "./Ordinary";
+import { joinPeriodic } from "./Periodic";
+import { RoomPeriodicConfigModel } from "../../../model/room/RoomPeriodicConfig";
+import { Result } from "./Type";
+
+export const join = async (
+    req: PatchRequest<{
+        Body: JoinBody;
+    }>,
+    reply: FastifyReply,
+): Promise<void> => {
+    const { roomUUID } = req.body;
+    const { userUUID } = req.user;
+
+    try {
+        const uuidIsPeriodicUUID = await getRepository(RoomPeriodicConfigModel).findOne({
+            select: ["id"],
+            where: {
+                periodic_uuid: roomUUID,
+            },
+        });
+
+        let result: Result;
+        if (uuidIsPeriodicUUID) {
+            result = await joinPeriodic(roomUUID, userUUID);
+        } else {
+            result = await joinOrdinary(roomUUID, userUUID);
+        }
+
+        return reply.send(result);
+    } catch (e) {
+        console.error(e);
+        return reply.send({
+            status: Status.Failed,
+            message: "Join room failed",
+        });
+    }
+};
+
+interface JoinBody {
+    roomUUID: string;
+}
+
+export const joinSchemaType: FastifySchema<{
+    body: JoinBody;
+}> = {
+    body: {
+        type: "object",
+        required: ["roomUUID"],
+        properties: {
+            roomUUID: {
+                type: "string",
+                maxLength: 40,
+            },
+        },
+    },
+};
