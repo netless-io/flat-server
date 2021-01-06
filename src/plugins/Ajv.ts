db420e469f12bf4a491022dde50e9708b94b8201
Black-Hole <158blackhole@gmail.com>
2021-01-06 15:03:49 +0800

    fix(ajv): unix-timestamp format invalid (#115)
    [ci skip]

--- src/plugins/Ajv.ts
+++ src/plugins/Ajv.ts
@@ -5,6 +5,7 @@ import Ajv, { FormatDefinition } from "ajv";
 // link: https://github.com/ajv-validator/ajv/blob/cd7c6a8385464f818814ebb1e84cc703dc7ef02c/docs/api.md#ajvaddformatname-string-format-format-ajv
 // link: https://github.com/ajv-validator/ajv-formats/blob/ce49433448384b4c0b2407adafc345e43b85f8ea/src/formats.ts#L38
 const unixTimestamp: FormatDefinition<number> = {
+    type: "number",
     validate: data => {
         return isValid(data);
     },
