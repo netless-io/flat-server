fa10dc4ccc9f44d84cd17fdba4a5fbbd4157b8e0
BlackHole1 <158blackhole@gmail.com>
2021-01-22 18:28:19 +0800

    refactor(dao): support distinct in find DAO

--- src/v1/dao/Implement.ts
+++ src/v1/dao/Implement.ts
@@ -7,16 +7,24 @@ export const DAOImplement: DAO<Model> = model => {
         const managerOrRepo = transaction || getRepository(model);
 
         return {
-            find: (select, where, order) => {
-                if (order) {
-                    return getRepository(model)
+            find: (select, where, config) => {
+                if (config?.order || config?.distinct) {
+                    let result = getRepository(model)
                         .createQueryBuilder()
                         .select(select)
                         .where({
                             ...noDelete(where),
-                        })
-                        .orderBy(order[0], order[1])
-                        .getRawMany();
+                        });
+
+                    if (config.distinct) {
+                        result = result.distinct(true);
+                    }
+
+                    if (config.order) {
+                        result = result.orderBy(config.order[0], config.order[1]);
+                    }
+
+                    return result.getRawMany();
                 }
 
                 return getRepository(model).find({
