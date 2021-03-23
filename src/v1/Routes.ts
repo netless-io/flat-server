7668a7ca91bb9757d61eb03572405f2774476b1f
Black-Hole1 <158blackhole@gmail.com>
2021-03-23 10:38:10 +0800

    fix(oss): miss alibaba cloud oss router and typo and useless alibaba cloud oss policy env

--- src/v1/Routes.ts
+++ src/v1/Routes.ts
@@ -2,9 +2,11 @@ import { FastifyRoutes } from "./types/Server";
 import { httpLogin } from "./controller/login/Router";
 import { httpAgora } from "./controller/agora/Router";
 import { httpRoom } from "./controller/room/Router";
+import { httpCloudStorage } from "./controller/cloudStorage/Router";
 
 export const httpRoutes: Readonly<Readonly<FastifyRoutes[]>[]> = Object.freeze([
     httpLogin,
     httpAgora,
     httpRoom,
+    httpCloudStorage,
 ]);
