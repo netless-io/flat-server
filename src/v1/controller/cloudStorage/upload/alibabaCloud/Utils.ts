4b14cbe0656bf47cf80a8fb4e96d1662b94d7ca0
Black-Hole1 <158blackhole@gmail.com>
2021-03-23 13:24:31 +0800

    fix(oss): typo alibaba cloud oss env

--- src/v1/controller/cloudStorage/upload/alibabaCloud/Utils.ts
+++ src/v1/controller/cloudStorage/upload/alibabaCloud/Utils.ts
@@ -15,7 +15,7 @@ const sts = new STS({
 // https://www.alibabacloud.com/help/zh/doc-detail/32077.htm?spm=a2c63.p38356.879954.20.24667727cDnb0Y#section-zkq-3rq-dhb
 export const alibabaCloudGetSTSToken = async (): Promise<Credentials> => {
     const { credentials } = await sts.assumeRole(
-        process.env.ALIBABA_CLOUD_OSS_OSS_ROLE_ARN, // role arn
+        process.env.ALIBABA_CLOUD_OSS_ROLE_ARN, // role arn
         undefined,
         15 * 60, // expiration
         process.env.ALIBABA_CLOUD_OSS_SESSION_NAME, // session name
