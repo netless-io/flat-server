b421048c9b74e05be2da3d5035051e76feb887c3
Cheerego7 <48879533+Cheerego7@users.noreply.github.com>
2021-03-24 17:52:19 +0800

    feat(whiteboard): add convert methods in utils (#283)

--- src/v1/utils/request/whiteboard/Whiteboard.ts
+++ src/v1/utils/request/whiteboard/Whiteboard.ts
@@ -1,5 +1,5 @@
 import { ax } from "../../Axios";
-import { shuntCreateRoomURL } from "./WhiteboardURL";
+import { shuntConversionTaskURL, shuntCreateRoomURL } from "./WhiteboardURL";
 import { createWhiteboardSDKToken } from "../../../../utils/NetlessToken";
 import { AxiosResponse } from "axios";
 
@@ -42,6 +42,75 @@ export const whiteboardBanRoom = async (uuid: string): Promise<AxiosResponse<Roo
         },
     );
 };
+
+export const whiteboardCreateConversionTask = async (
+    body: CreateConversionTaskParams,
+): Promise<AxiosResponse<TaskCreated>> => {
+    return await ax.post<TaskCreated>(shuntConversionTaskURL, body, {
+        headers: {
+            token: createWhiteboardSDKToken(),
+            // TODO region: 'cn-hz',
+        },
+    });
+};
+
+export const whiteboardQueryConversionTask = async (
+    uuid: string,
+    type: "static" | "dynamic",
+): Promise<AxiosResponse<TaskStatus>> => {
+    return await ax.get<TaskStatus>(`${shuntConversionTaskURL}/${uuid}?type=${type}`, {
+        headers: {
+            token: createWhiteboardSDKToken(),
+            // TODO region: 'cn-hz',
+        },
+    });
+};
+
+type CreateConversionTaskParams =
+    | CreateStaticConversionTaskParams
+    | CreateDynamicConversionTaskParams;
+
+interface CreateStaticConversionTaskParams {
+    resource: string;
+    type: "static";
+    /** @default 1.2 */
+    scale?: number;
+    /** @default 'png' */
+    outputFormat?: "png" | "jpg" | "jpeg" | "webp";
+    pack?: boolean;
+}
+
+interface CreateDynamicConversionTaskParams {
+    resource: string;
+    type: "dynamic";
+    /** @default false */
+    preview?: boolean;
+}
+
+interface TaskCreated {
+    uuid: string;
+    type: "static" | "dynamic";
+    status: "Waiting" | "Converting" | "Finished" | "Fail";
+}
+
+interface TaskStatus {
+    uuid: string;
+    type: "static" | "dynamic";
+    status: "Waiting" | "Converting" | "Finished" | "Fail";
+    failedReason: string;
+    progress: {
+        totalPageSize: number;
+        convertedPageSize: number;
+        convertedPercentage: number;
+        convertedFileList: {
+            width: number;
+            height: number;
+            conversionFileUrl: string;
+            preview?: string;
+        }[];
+        currentStep: "Extracting" | "Packaging" | "GeneratingPreview" | "MediaTranscode";
+    };
+}
 
 interface Room {
     uuid: string;
