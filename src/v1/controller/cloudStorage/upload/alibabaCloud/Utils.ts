20c0a89ddc98732d9908ef226d38eeb7a76d98c3
Cheerego7 <48879533+Cheerego7@users.noreply.github.com>
2021-03-30 17:35:50 +0800

    feat(cloud-storage): add remove api in alibaba cloud oss (#294)

--- src/v1/controller/cloudStorage/upload/alibabaCloud/Utils.ts
+++ src/v1/controller/cloudStorage/upload/alibabaCloud/Utils.ts
@@ -59,11 +59,12 @@ export const getOSSFileURLPath = (filePath: string): string => {
     return `https://${AlibabaCloud.OSS_BUCKET}.${AlibabaCloud.OSS_REGION}.aliyuncs.com/${filePath}`;
 };
 
-const ossClient = new OSS({
+export const ossClient = new OSS({
     bucket: AlibabaCloud.OSS_BUCKET,
     region: AlibabaCloud.OSS_REGION,
     accessKeyId: AlibabaCloud.OSS_ACCESS_KEY,
     accessKeySecret: AlibabaCloud.OSS_ACCESS_KEY_SECRET,
+    secure: true,
 });
 
 /**
