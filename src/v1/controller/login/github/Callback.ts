93d0fa95002f4bcd4385efbdbc25c81e1aecfa01
BlackHole1 <158blackhole@gmail.com>
2021-04-30 20:57:21 +0800

    refactor(login): improve github callback page

--- src/v1/controller/login/github/Callback.ts
+++ src/v1/controller/login/github/Callback.ts
@@ -26,7 +26,7 @@ export const callback: Controller<CallbackRequest, any> = async ({ req, logger }
 
             await redisService.set(RedisKey.authFailed(authUUID), "", 60 * 60);
 
-            return reply.send("Failed");
+            return reply.send(failedHTML);
         }
 
         const accessToken = await getGithubAccessToken(code, authUUID);
@@ -85,11 +85,11 @@ export const callback: Controller<CallbackRequest, any> = async ({ req, logger }
             60 * 60,
         );
 
-        return reply.send("Success");
+        return reply.send(successHTML);
     } catch (err: unknown) {
         logger.error("request failed", parseError(err));
         await redisService.set(RedisKey.authFailed(authUUID), "", 60 * 60);
-        return reply.send("Failed");
+        return reply.send(failedHTML);
     }
 };
 
@@ -115,3 +115,36 @@ export const callbackSchemaType: FastifySchema<CallbackRequest> = {
         },
     },
 };
+
+const successHTML = `<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Success</title>
+</head>
+<body>
+    <svg style=max-width:40vw;max-height:40vh;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%) viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg">
+        <path d="M40 0c22.0914 0 40 17.9086 40 40S62.0914 80 40 80 0 62.0914 0 40 17.9086 0 40 0zm0 4C20.1177 4 4 20.1177 4 40s16.1177 36 36 36 36-16.1177 36-36S59.8823 4 40 4zm22.6337 20.5395l2.7326 2.921-32.3889 30.2993L14.61 40.0046l2.78-2.876L33.022 52.24l29.6117-27.7005z" fill=#9FDF76 fill-rule=nonzero />
+    </svg>
+</body>
+<script>
+    setTimeout(close, 3e3);
+</script>
+</html>
+`;
+
+const failedHTML = `<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Failed</title>
+</head>
+<body>
+    <svg style=max-width:40vw;max-height:40vh;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%) viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg">
+        <path d="M40 0c22.0914 0 40 17.9086 40 40S62.0914 80 40 80 0 62.0914 0 40 17.9086 0 40 0zm0 4C20.1177 4 4 20.1177 4 40s16.1177 36 36 36 36-16.1177 36-36S59.8823 4 40 4zm21.0572 49.2345l.357.3513-2.8284 2.8284c-10.162-10.162-26.5747-10.2636-36.8617-.3048l-.3099.3048-2.8284-2.8284c11.7085-11.7085 30.619-11.8256 42.4714-.3513zM27 26c2.2091 0 4 1.7909 4 4 0 2.2091-1.7909 4-4 4-2.2091 0-4-1.7909-4-4 0-2.2091 1.7909-4 4-4zm26 0c2.2091 0 4 1.7909 4 4 0 2.2091-1.7909 4-4 4-2.2091 0-4-1.7909-4-4 0-2.2091 1.7909-4 4-4z" fill=#F45454 fill-rule=nonzero />
+    </svg>
+</body>
+<script>
+    setTimeout(close, 3e3);
+</script>
+</html>`;
