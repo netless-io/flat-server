f44ae2856136fe879832e07284a1ec79e64cedd1
Black-Hole <158blackhole@gmail.com>
2021-06-10 14:20:17 +0800

    refactor(login): don't launch flat app when login github from web (#349)

--- src/v1/controller/login/github/Callback.ts
+++ src/v1/controller/login/github/Callback.ts
@@ -16,7 +16,7 @@ export const callback: Controller<CallbackRequest, any> = async ({ req, logger }
         "content-type": "text/html",
     });
 
-    const { state: authUUID } = req.query;
+    const { state: authUUID, platform } = req.query;
 
     try {
         if ("error" in req.query) {
@@ -108,7 +108,7 @@ export const callback: Controller<CallbackRequest, any> = async ({ req, logger }
             60 * 60,
         );
 
-        return reply.send(successHTML);
+        return reply.send(successHTML(platform !== "web"));
     } catch (err: unknown) {
         logger.error("request failed", parseError(err));
         await redisService.set(RedisKey.authFailed(authUUID), "", 60 * 60);
@@ -119,6 +119,7 @@ export const callback: Controller<CallbackRequest, any> = async ({ req, logger }
 interface CallbackRequest {
     querystring: {
         state: string;
+        platform?: "web";
     } & (
         | {
               code: string;
@@ -144,6 +145,10 @@ export const callbackSchemaType: FastifySchema<CallbackRequest> = {
             error: {
                 type: "string",
             },
+            platform: {
+                type: "string",
+                nullable: true,
+            },
         },
         oneOf: [
             {
@@ -156,7 +161,14 @@ export const callbackSchemaType: FastifySchema<CallbackRequest> = {
     },
 };
 
-const successHTML = `<!doctype html>
+const successHTML = (needLaunchApp: boolean): string => {
+    const launchAppCode = needLaunchApp
+        ? `setTimeout(() => {
+            location.href = "x-agora-flat-client://open"
+        }, 1000 * 3)`
+        : "";
+
+    return `<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -166,15 +178,18 @@ const successHTML = `<!doctype html>
     <svg style=max-width:80px;max-height:80px;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%) viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg">
         <path d="M40 0c22.0914 0 40 17.9086 40 40S62.0914 80 40 80 0 62.0914 0 40 17.9086 0 40 0zm0 4C20.1177 4 4 20.1177 4 40s16.1177 36 36 36 36-16.1177 36-36S59.8823 4 40 4zm22.6337 20.5395l2.7326 2.921-32.3889 30.2993L14.61 40.0046l2.78-2.876L33.022 52.24l29.6117-27.7005z" fill=#9FDF76 fill-rule=nonzero />
     </svg>
-    <div id=a style=position:fixed;top:60%;left:50%;transform:translate(-50%,-50%)>Login Success</div>
+    <div id=text style=position:fixed;top:60%;left:50%;transform:translate(-50%,-50%)>Login Success</div>
 </body>
 <script>
-    if (navigator.language.startsWith('zh'))
-        document.getElementById('a').textContent = '登录成功'
-    setTimeout(function(){location.href='x-agora-flat-client://open'}, 1e3)
+    if (navigator.language.startsWith("zh")) {
+        document.getElementById("text").textContent = "登录成功"
+    }
+
+    ${launchAppCode}
 </script>
 </html>
 `;
+};
 
 const failedHTML = `<!doctype html>
 <html lang="en">
@@ -186,10 +201,11 @@ const failedHTML = `<!doctype html>
     <svg style=max-width:80px;max-height:80px;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%) viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg">
         <path d="M40 0c22.0914 0 40 17.9086 40 40S62.0914 80 40 80 0 62.0914 0 40 17.9086 0 40 0zm0 4C20.1177 4 4 20.1177 4 40s16.1177 36 36 36 36-16.1177 36-36S59.8823 4 40 4zm21.0572 49.2345l.357.3513-2.8284 2.8284c-10.162-10.162-26.5747-10.2636-36.8617-.3048l-.3099.3048-2.8284-2.8284c11.7085-11.7085 30.619-11.8256 42.4714-.3513zM27 26c2.2091 0 4 1.7909 4 4 0 2.2091-1.7909 4-4 4-2.2091 0-4-1.7909-4-4 0-2.2091 1.7909-4 4-4zm26 0c2.2091 0 4 1.7909 4 4 0 2.2091-1.7909 4-4 4-2.2091 0-4-1.7909-4-4 0-2.2091 1.7909-4 4-4z" fill=#F45454 fill-rule=nonzero />
     </svg>
-    <div id=a style=position:fixed;top:60%;left:50%;transform:translate(-50%,-50%)>Login Failed</div>
+    <div id=text style=position:fixed;top:60%;left:50%;transform:translate(-50%,-50%)>Login Failed</div>
 <script>
-    if (navigator.language.startsWith('zh'))
-        document.getElementById('a').textContent = '登录失败'
+    if (navigator.language.startsWith("zh")) {
+        document.getElementById("text").textContent = "登录失败"
+    }
 </script>
 </body>
 </html>
