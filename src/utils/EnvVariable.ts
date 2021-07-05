995ecbb75e54379ef3811c059d7a0c8b5acfe466
Black-Hole <158blackhole@gmail.com>
2021-07-05 17:13:59 +0800

    fix(logger): project dir value incorrect (#357)

--- src/utils/EnvVariable.ts
+++ src/utils/EnvVariable.ts
@@ -1,12 +1,12 @@
 import os from "os";
 import path from "path";
-import { isDev } from "../constants/Process";
 import { format } from "date-fns/fp";
 import { EnvVariableParse } from "./EnvVariableParse";
 
 export const variables = {
     HOSTNAME: os.hostname(),
-    PROJECT_DIR: isDev ? path.resolve(__dirname, "..") : path.resolve(__dirname, "..", ".."),
+    // __dirname: dist/
+    PROJECT_DIR: path.resolve(__dirname, ".."),
     // eslint-disable-next-line @typescript-eslint/naming-convention
     DAY_DATE: (): string => format("yyyy-MM-dd")(Date.now()),
 };
