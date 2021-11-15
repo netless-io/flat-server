1dc03828dc2bf07af34303d25db93f0795b5987b
berryhe <hebianyu@agora.io>
2021-11-15 18:46:24 +0800

    chore(cache): open conn

--- pkg/cache/redis/redis.go
+++ pkg/cache/redis/redis.go
@@ -1,1 +1,24 @@
 package redis
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/netless-io/flat-server/conf"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var redisClient *redis.Client
+
+func OpenConn(conf conf.RedisConf) error {
+	addr := fmt.Sprintf("%s:%v", conf.Host, conf.Port)
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: conf.Password,
+		DB:       conf.Pool,
+	})
+
+	// testing conn
+	return redisClient.Ping(context.Background()).Err()
+}
