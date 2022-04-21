a2456b1748d5ccd4ddfc7e6d3a6766e22510d2c3
Black-Hole <158blackhole@gmail.com>
2022-04-21 16:46:06 +0800

    fix(api): forget delete account router (#512)

--- src/v1/controller/user/Router.ts
+++ src/v1/controller/user/Router.ts
@@ -3,10 +3,12 @@ import { Rename } from "./rename";
 import { SendMessage } from "./bindingPhone/SendMessage";
 import { BindingPhone } from "./bindingPhone/Binding";
 import { DeleteAccountValidate } from "./deleteAccount/Validate";
+import { DeleteAccount } from "./deleteAccount";
 
 export const userRouters: Readonly<Array<ControllerClass<any, any>>> = Object.freeze([
     Rename,
     SendMessage,
     BindingPhone,
     DeleteAccountValidate,
+    DeleteAccount,
 ]);
