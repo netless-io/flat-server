6a619f84c4ba3886eeb17e52d68fd295f560c481
Black-Hole <158blackhole@gmail.com>
2022-06-07 15:35:48 +0800

    fix(login): new user register failed (#560)

--- src/v1/service/cloudStorage/CloudStorageFiles.ts
+++ src/v1/service/cloudStorage/CloudStorageFiles.ts
@@ -2,6 +2,7 @@ import { EntityManager } from "typeorm/entity-manager/EntityManager";
 import { CloudStorageFilesDAO } from "../../../dao";
 import { InsertResult } from "typeorm";
 import { FilePayload } from "../../../model/cloudStorage/Types";
+import { FileResourceType } from "../../../model/cloudStorage/Constants";
 
 export class ServiceCloudStorageFiles {
     public constructor() {}
@@ -13,16 +14,18 @@ export class ServiceCloudStorageFiles {
             fileURL: string;
             fileUUID: string;
             payload: FilePayload;
+            resourceType: FileResourceType;
         },
         t?: EntityManager,
     ): Promise<InsertResult> {
-        const { fileName, fileSize, fileURL, fileUUID, payload } = data;
+        const { fileName, fileSize, fileURL, fileUUID, payload, resourceType } = data;
         return CloudStorageFilesDAO(t).insert({
             file_name: fileName,
             file_size: fileSize,
             file_url: fileURL,
             file_uuid: fileUUID,
             payload,
+            resource_type: resourceType,
         });
     }
 }
