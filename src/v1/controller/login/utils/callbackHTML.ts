e6ed2368498dfd8d2e8c071613b7c21a1241cdaf
BlackHole1 <158blackhole@gmail.com>
2022-06-16 16:22:54 +0800

    feat(binding): support binding github

--- src/v1/controller/login/utils/callbackHTML.ts
+++ src/v1/controller/login/utils/callbackHTML.ts
@@ -1,4 +1,4 @@
-export const successHTML = (needLaunchApp: boolean): string => {
+export const successHTML = (needLaunchApp: boolean, isBinding = false): string => {
     const launchAppCode = needLaunchApp
         ? `setTimeout(() => {
             location.href = "x-agora-flat-client://open"
@@ -15,11 +15,15 @@ export const successHTML = (needLaunchApp: boolean): string => {
                 <svg style=max-width:80px;max-height:80px;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%) viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg">
                     <path d="M40 0c22.0914 0 40 17.9086 40 40S62.0914 80 40 80 0 62.0914 0 40 17.9086 0 40 0zm0 4C20.1177 4 4 20.1177 4 40s16.1177 36 36 36 36-16.1177 36-36S59.8823 4 40 4zm22.6337 20.5395l2.7326 2.921-32.3889 30.2993L14.61 40.0046l2.78-2.876L33.022 52.24l29.6117-27.7005z" fill="#9FDF76" fill-rule="nonzero" />
                 </svg>
-                <div id="text" style=position:fixed;top:60%;left:50%;transform:translate(-50%,-50%)>Login Success</div>
+                <div id="text" style=position:fixed;top:60%;left:50%;transform:translate(-50%,-50%)>${
+                    isBinding ? "Binding Success" : "Login Success"
+                }</div>
             </body>
             <script>
                 if (navigator.language.startsWith("zh")) {
-                    document.getElementById("text").textContent = "登录成功"
+                    document.getElementById("text").textContent = ${
+                        isBinding ? "绑定成功" : "登录成功"
+                    }
                 }
                 ${launchAppCode}
             </script>
@@ -27,7 +31,7 @@ export const successHTML = (needLaunchApp: boolean): string => {
         `;
 };
 
-export const failedHTML = (): string => {
+export const failedHTML = (isBinding = false): string => {
     return `<!doctype html>
             <html lang="en">
             <head>
@@ -38,10 +42,14 @@ export const failedHTML = (): string => {
                 <svg style=max-width:80px;max-height:80px;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%) viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg">
                     <path d="M40 0c22.0914 0 40 17.9086 40 40S62.0914 80 40 80 0 62.0914 0 40 17.9086 0 40 0zm0 4C20.1177 4 4 20.1177 4 40s16.1177 36 36 36 36-16.1177 36-36S59.8823 4 40 4zm21.0572 49.2345l.357.3513-2.8284 2.8284c-10.162-10.162-26.5747-10.2636-36.8617-.3048l-.3099.3048-2.8284-2.8284c11.7085-11.7085 30.619-11.8256 42.4714-.3513zM27 26c2.2091 0 4 1.7909 4 4 0 2.2091-1.7909 4-4 4-2.2091 0-4-1.7909-4-4 0-2.2091 1.7909-4 4-4zm26 0c2.2091 0 4 1.7909 4 4 0 2.2091-1.7909 4-4 4-2.2091 0-4-1.7909-4-4 0-2.2091 1.7909-4 4-4z" fill="#F45454" fill-rule="nonzero" />
                 </svg>
-                <div id="text" style=position:fixed;top:60%;left:50%;transform:translate(-50%,-50%)>Login Failed</div>
+                <div id="text" style=position:fixed;top:60%;left:50%;transform:translate(-50%,-50%)>${
+                    isBinding ? "Binding Failed" : "Login Failed"
+                }</div>
             <script>
                 if (navigator.language.startsWith("zh")) {
-                    document.getElementById("text").textContent = "登录失败"
+                    document.getElementById("text").textContent = ${
+                        isBinding ? "绑定失败" : "登录失败"
+                    }
                 }
             </script>
             </body>
---
+++ src/v1/controller/user/binding/platform/github/Binding.ts
@@ -0,0 +1,96 @@
+import { Controller } from "../../../../../../decorator/Controller";
+import { Github } from "../../../../../../constants/Config";
+import { AbstractController } from "../../../../../../abstract/controller";
+import { FastifySchema, ResponseError } from "../../../../../../types/Server";
+import redisService from "../../../../../../thirdPartyService/RedisService";
+import RedisService from "../../../../../../thirdPartyService/RedisService";
+import { RedisKey } from "../../../../../../utils/Redis";
+import { ControllerError } from "../../../../../../error/ControllerError";
+import { ErrorCode } from "../../../../../../ErrorCode";
+import { failedHTML, successHTML } from "../../../../login/utils/callbackHTML";
+import { LoginGithub } from "../../../../login/platforms/LoginGithub";
+import { ServiceUserGithub } from "../../../../../service/user/UserGithub";
+import { parseError } from "../../../../../../logger";
+
+@Controller<RequestType, any>({
+    method: "get",
+    path: "user/binding/platform/github",
+    auth: false,
+    skipAutoHandle: true,
+    enable: Github.enable,
+})
+export class BindingGithub extends AbstractController<RequestType, any> {
+    public static readonly schema: FastifySchema<RequestType> = {
+        querystring: {
+            type: "object",
+            required: ["state"],
+            properties: {
+                state: {
+                    type: "string",
+                    format: "uuid-v4",
+                },
+                code: {
+                    type: "string",
+                },
+                error: {
+                    type: "string",
+                    nullable: true,
+                },
+            },
+        },
+    };
+
+    public async execute(): Promise<void> {
+        void this.reply.headers({
+            "content-type": "text/html",
+        });
+
+        const { state: authUUID, code } = this.querystring;
+
+        BindingGithub.assertCallbackParamsNoError(this.querystring);
+
+        const userUUID = await RedisService.get(RedisKey.bindingAuthUUID(authUUID));
+        if (userUUID === null) {
+            this.logger.warn("uuid verification failed");
+            throw new ControllerError(ErrorCode.ParamsCheckFailed);
+        }
+
+        const userInfo = await LoginGithub.getUserInfoAndToken(code, authUUID);
+        const userUUIDByDB = await ServiceUserGithub.userUUIDByUnionUUID(userInfo.unionUUID);
+        if (userUUIDByDB) {
+            throw new ControllerError(ErrorCode.UserAlreadyBinding);
+        }
+
+        const githubSVC = new ServiceUserGithub(userUUID);
+        await githubSVC.create(userInfo);
+
+        await redisService.set(RedisKey.bindingAuthStatus(this.querystring.state), "true", 60 * 60);
+
+        return this.reply.send(successHTML(false, true));
+    }
+
+    private static assertCallbackParamsNoError(querystring: RequestType["querystring"]): void {
+        if (querystring.error) {
+            throw new Error("callback query params did not pass the github check");
+        }
+    }
+
+    public async errorHandler(error: Error): Promise<ResponseError> {
+        await redisService.set(
+            RedisKey.bindingAuthStatus(this.querystring.state),
+            "false",
+            60 * 60,
+        );
+
+        this.logger.error("request failed", parseError(error));
+        return this.reply.send(failedHTML(true));
+    }
+}
+
+interface RequestType {
+    querystring: {
+        state: string;
+        code: string;
+        error?: string;
+    };
+}
