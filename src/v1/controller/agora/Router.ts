9dcae169e2bc80df3e5fc185f2ecc0671963c89f
Black-Hole <158blackhole@gmail.com>
2022-07-04 15:04:05 +0800

    feat(rtm): support rtm message censor (#594)

--- src/v1/controller/agora/Router.ts
+++ src/v1/controller/agora/Router.ts
@@ -2,9 +2,11 @@ import { GenerateRTC } from "./token/RTC";
 import { GenerateRTM } from "./token/RTM";
 import { ControllerClass } from "../../../abstract/controller";
 import { MessageCallback } from "./message/Callback";
+import { RTMCensor } from "./rtm/censor";
 
 export const agoraRouters: Readonly<Array<ControllerClass<any, any>>> = Object.freeze([
     GenerateRTC,
     GenerateRTM,
     MessageCallback,
+    RTMCensor,
 ]);
