1369a352e2f8534ae577d9b609b9dc0c4331c972
BlackHole1 <158blackhole@gmail.com>
2022-07-14 16:15:14 +0800

    fix(html): login and binding html failed

--- src/v1/controller/login/utils/callbackHTML.ts
+++ src/v1/controller/login/utils/callbackHTML.ts
@@ -22,7 +22,7 @@ export const successHTML = (needLaunchApp: boolean, isBinding = false): string =
             <script>
                 if (navigator.language.startsWith("zh")) {
                     document.getElementById("text").textContent = ${
-                        isBinding ? "绑定成功" : "登录成功"
+                        isBinding ? "'绑定成功'" : "'登录成功'"
                     }
                 }
                 ${launchAppCode}
@@ -43,12 +43,12 @@ export const failedHTML = (isBinding = false): string => {
                     <path d="M40 0c22.0914 0 40 17.9086 40 40S62.0914 80 40 80 0 62.0914 0 40 17.9086 0 40 0zm0 4C20.1177 4 4 20.1177 4 40s16.1177 36 36 36 36-16.1177 36-36S59.8823 4 40 4zm21.0572 49.2345l.357.3513-2.8284 2.8284c-10.162-10.162-26.5747-10.2636-36.8617-.3048l-.3099.3048-2.8284-2.8284c11.7085-11.7085 30.619-11.8256 42.4714-.3513zM27 26c2.2091 0 4 1.7909 4 4 0 2.2091-1.7909 4-4 4-2.2091 0-4-1.7909-4-4 0-2.2091 1.7909-4 4-4zm26 0c2.2091 0 4 1.7909 4 4 0 2.2091-1.7909 4-4 4-2.2091 0-4-1.7909-4-4 0-2.2091 1.7909-4 4-4z" fill="#F45454" fill-rule="nonzero" />
                 </svg>
                 <div id="text" style=position:fixed;top:60%;left:50%;transform:translate(-50%,-50%)>${
-                    isBinding ? "Binding Failed" : "Login Failed"
+                    isBinding ? "'Binding Failed'" : "'Login Failed'"
                 }</div>
             <script>
                 if (navigator.language.startsWith("zh")) {
                     document.getElementById("text").textContent = ${
-                        isBinding ? "绑定失败" : "登录失败"
+                        isBinding ? "'绑定失败'" : "'登录失败'"
                     }
                 }
             </script>
