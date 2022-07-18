e623564b2dabaff04b966bfd4504ae8f4c99543f
Black-Hole <158blackhole@gmail.com>
2022-07-18 14:10:29 +0800

    refactor(api): improve v2 cloud-storage api (#621)

--- src/v2/controllers/cloud-storage/routes.ts
+++ src/v2/controllers/cloud-storage/routes.ts
@@ -1,18 +1,18 @@
 import { Server } from "../../../utils/registryRoutersV2";
 import { cloudStorageList, cloudStorageListSchema } from "./list";
-import { cloudStorageDirectoryCreate, cloudStorageDirectoryCreateSchema } from "./directory/create";
-import { cloudStorageDirectoryRename, cloudStorageDirectoryRenameSchema } from "./directory/rename";
+import { cloudStorageDirectoryCreate, cloudStorageDirectoryCreateSchema } from "./create-directory";
+import { cloudStorageDirectoryRename, cloudStorageDirectoryRenameSchema } from "./rename";
 
 export const cloudStorageRouters = (server: Server): void => {
     server.post("cloud-storage/list", cloudStorageList, {
         schema: cloudStorageListSchema,
     });
 
-    server.post("cloud-storage/directory/create", cloudStorageDirectoryCreate, {
+    server.post("cloud-storage/create-directory", cloudStorageDirectoryCreate, {
         schema: cloudStorageDirectoryCreateSchema,
     });
 
-    server.post("cloud-storage/directory/rename", cloudStorageDirectoryRename, {
+    server.post("cloud-storage/rename", cloudStorageDirectoryRename, {
         schema: cloudStorageDirectoryRenameSchema,
     });
 };
