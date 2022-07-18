d72bb4427faa1b9449f161ca8d6a661294075d4e
BlackHole1 <158blackhole@gmail.com>
2022-07-18 13:40:19 +0800

    fix(api): forget add dir rename api

--- src/v2/controllers/cloud-storage/routes.ts
+++ src/v2/controllers/cloud-storage/routes.ts
@@ -1,6 +1,7 @@
 import { Server } from "../../../utils/registryRoutersV2";
 import { cloudStorageList, cloudStorageListSchema } from "./list";
 import { cloudStorageDirectoryCreate, cloudStorageDirectoryCreateSchema } from "./directory/create";
+import { cloudStorageDirectoryRename, cloudStorageDirectoryRenameSchema } from "./directory/rename";
 
 export const cloudStorageRouters = (server: Server): void => {
     server.post("cloud-storage/list", cloudStorageList, {
@@ -9,5 +10,9 @@ export const cloudStorageRouters = (server: Server): void => {
 
     server.post("cloud-storage/directory/create", cloudStorageDirectoryCreate, {
         schema: cloudStorageDirectoryCreateSchema,
+    });
+
+    server.post("cloud-storage/directory/rename", cloudStorageDirectoryRename, {
+        schema: cloudStorageDirectoryRenameSchema,
     });
 };
