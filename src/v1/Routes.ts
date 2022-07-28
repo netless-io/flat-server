08b65e623a50343e6cea44080c146b10fb077724
BlackHole1 <158blackhole@gmail.com>
2022-07-28 15:12:39 +0800

    feat(api): add log api
    # Conflicts:
    #	src/v1/Routes.ts

--- src/v1/Routes.ts
+++ src/v1/Routes.ts
@@ -5,13 +5,16 @@ import { cloudStorageRouters } from "./controller/cloudStorage/Router";
 import { ControllerClass } from "../abstract/controller";
 import { userRouters } from "./controller/user/Router";
 import { aliCloudRouters } from "./controller/aliCloud/Router";
+import { logRouters } from "./controller/logger/Router";
 
-export const httpRouters: Readonly<Array<Readonly<Array<ControllerClass<any, any>>>>> =
-    Object.freeze([
-        agoraRouters,
-        aliCloudRouters,
-        loginRouters,
-        roomRouters,
-        cloudStorageRouters,
-        userRouters,
-    ]);
+export const httpRouters: Readonly<
+    Array<Readonly<Array<ControllerClass<any, any>>>>
+> = Object.freeze([
+    agoraRouters,
+    aliCloudRouters,
+    loginRouters,
+    roomRouters,
+    cloudStorageRouters,
+    logRouters,
+    userRouters,
+]);
