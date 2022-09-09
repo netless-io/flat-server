76a5fcd8f4cc0ef52844b552942d92a6548d2cb0
Black-Hole <158blackhole@gmail.com>
2022-09-09 17:52:19 +0800

    fix(api): forget add v2 user routes (#676)

--- src/v2/controllers/routes.ts
+++ src/v2/controllers/routes.ts
@@ -1,3 +1,4 @@
 import { cloudStorageRouters } from "./cloud-storage/routes";
+import { userRouters } from "./user/routes";
 
-export const v2Routes = [cloudStorageRouters];
+export const v2Routes = [userRouters, cloudStorageRouters];
