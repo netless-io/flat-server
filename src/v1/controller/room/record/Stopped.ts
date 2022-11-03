8f89af8db36e81d55bf75f1fb1fd3f29b7674703
xuyunshi <405029644@qq.com>
2022-11-03 18:16:44 +0800

    Revert "fix(room-record): move rewrite function to agora stop (#697)"
    This reverts commit aa4889412ff704a0982717449d436c8cb94229e8.

--- src/v1/controller/room/record/Stopped.ts
+++ src/v1/controller/room/record/Stopped.ts
@@ -5,6 +5,12 @@ import { RoomDAO, RoomRecordDAO } from "../../../../dao";
 import { roomIsRunning } from "../utils/RoomStatus";
 import { Controller } from "../../../../decorator/Controller";
 import { AbstractController } from "../../../../abstract/controller";
+import { dataSource } from "../../../../thirdPartyService/TypeORMService";
+import { patchForM3U8 } from "./PatchForAgoraM3U8";
+import { RoomUserModel } from "../../../../model/room/RoomUser";
+import { Agora } from "../../../../constants/Config";
+import { isExistObject } from "../../cloudStorage/alibabaCloud/Utils";
+
 @Controller<RequestType, ResponseType>({
     method: "post",
     // the logic here is consistent with room/record/stopped
@@ -28,6 +34,7 @@ export class RecordStopped extends AbstractController<RequestType, ResponseType>
     public async execute(): Promise<Response<ResponseType>> {
         const { roomUUID } = this.body;
         const userUUID = this.userUUID;
+
         const roomInfo = await RoomDAO().findOne(["room_status"], {
             room_uuid: roomUUID,
             owner_uuid: userUUID,
@@ -58,10 +65,45 @@ export class RecordStopped extends AbstractController<RequestType, ResponseType>
             1,
         );
 
+        await this.patchForAgoraRecordM3U8Files();
         return {
             status: Status.Success,
             data: {},
         };
+    }
+
+    async patchForAgoraRecordM3U8Files(): Promise<void> {
+        const { roomUUID } = this.body;
+        const roomUsersInfoBasic = dataSource
+            .createQueryBuilder(RoomUserModel, "ru")
+            .addSelect("ru.rtc_uid", "rtc_uid")
+            .andWhere("room_uuid = :roomUUID", {
+                roomUUID,
+            })
+            .andWhere("ru.is_delete = false");
+
+        const roomUsersRtcInfo = await roomUsersInfoBasic.getRawMany<RoomUsersRtcUIDInfo>();
+        const resourcePath = `/${Agora.ossFolder}/${roomUUID.replace(/-/g, "")}`;
+
+        const recordInfo = await RoomRecordDAO().findOne(["agora_sid"], {
+            room_uuid: roomUUID,
+        });
+        if (recordInfo === undefined) {
+            throw new Error("record info not found");
+        }
+        const allRoomUsersRtcRecordPath = roomUsersRtcInfo.map(({ rtc_uid }) => {
+            const agoraResourceURL = `${recordInfo.agora_sid}_${roomUUID}__uid_s_${rtc_uid}__uid_e_av.m3u8`;
+            return agoraResourceURL;
+        });
+        const existedUsersRecordPath = allRoomUsersRtcRecordPath.filter(async url => {
+            const fullPath = `${resourcePath}/${url}`;
+            const urlExist = await isExistObject(fullPath);
+            return urlExist;
+        });
+
+        for (const userPath of existedUsersRecordPath) {
+            await patchForM3U8(userPath, resourcePath);
+        }
     }
 
     public errorHandler(error: Error): ResponseError {
@@ -76,3 +118,5 @@ interface RequestType {
 }
 
 interface ResponseType {}
+
+type RoomUsersRtcUIDInfo = Pick<RoomUserModel, "rtc_uid">;
