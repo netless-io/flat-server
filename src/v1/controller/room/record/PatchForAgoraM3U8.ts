2caf7f8d64956116318b93a195e6c3244ee56162
xuyunshi <405029644@qq.com>
2022-11-03 18:16:52 +0800

    Revert "fix(room-record): update file path splicing (#696)"
    This reverts commit ff450de2cd153f6986572379222dd7f6c5054fe1.

--- src/v1/controller/room/record/PatchForAgoraM3U8.ts
+++ src/v1/controller/room/record/PatchForAgoraM3U8.ts
@@ -1,16 +1,24 @@
 /* eslint-disable no-continue */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+import { createWriteStream, mkdir, readdirSync, statSync, rmdirSync, unlinkSync } from "fs";
+import { join } from "path";
+import nReadLines from "n-readlines";
 import { isExistObject, ossClient } from "../../cloudStorage/alibabaCloud/Utils";
-import { Readable } from "stream";
 
 const mediaPrefixs = ["# Audio", "# Video"];
-async function patchLoopM3U8File(lines: string[], fileName: string, prefix: string): Promise<void> {
+const m3u8Rex = /[a-z0-9_-]*\.m3u8/g;
+async function patchLoopM3U8File(fileName: string, prefix: string, dirName: string): Promise<void> {
     let sequenceNum = 1;
-    const modifiedLines: string[] = [];
+    mkdir(dirName, () => {});
+    const outName = `./${dirName}/${fileName}`;
+    const file = new nReadLines(fileName);
+    const writeFile = createWriteStream(outName);
+
     let stagedLines: string[] = [];
-    for (let i = 0; i < lines.length; i++) {
-        const line = lines[i];
+    let lineData;
+    while ((lineData = file.next())) {
+        const line: string = lineData.toString();
         const startWithMediaPrefix =
             mediaPrefixs.filter(prefix => line.startsWith(prefix)).length > 0;
         if (startWithMediaPrefix && stagedLines.length === 0) {
@@ -19,67 +27,81 @@ async function patchLoopM3U8File(lines: string[], fileName: string, prefix: stri
         }
         if (stagedLines.length > 0) {
             stagedLines.push(line);
-            const rexResult = /[a-z0-9_-]*\.m3u8/g.exec(line);
+            const rexResult = m3u8Rex.exec(line);
             if (rexResult !== null && rexResult.length > 0) {
                 const m3u8_url = rexResult[0];
-                const ossPath = `${prefix}/${m3u8_url}`;
-                const exist = await isExistObject(ossPath);
+                const exist = await isExistObject(prefix + m3u8_url);
                 // If exist then write to file,
                 // if not just ignore it
                 if (exist) {
-                    void rewriteM3U8Discontinuity(m3u8_url, prefix, sequenceNum);
+                    void rewriteM3U8Discontinuity(m3u8_url, dirName, prefix, sequenceNum);
                     sequenceNum += 1;
-                    stagedLines.forEach(l => modifiedLines.push(l));
+                    stagedLines.forEach(l => writeFile.write(l + "\n"));
                 }
                 stagedLines = [];
             }
             continue;
         }
-        modifiedLines.push(line);
-    }
-    if (modifiedLines.length !== lines.length) {
-        const content = modifiedLines.join("\n");
-        await putStringToOss(`${prefix}/${fileName}`, content);
+        writeFile.write(`${line}\n`);
     }
 }
 
 const discontinuityString = "#EXT-X-DISCONTINUITY";
-const discontinuitySequenceString = "#EXT-X-DISCONTINUITY-SEQUENCE";
+const versionRex = /#EXT-X-VERSION:\d/g;
 async function rewriteM3U8Discontinuity(
     fileName: string,
+    dir: string,
     prefix: string,
     sequenceNum: number,
 ): Promise<void> {
-    const fullPath = `${prefix}/${fileName}`;
+    const fullPath = `${prefix}${fileName}`;
     const ossResult = await ossClient.get(fullPath);
     const fileContent = ossResult.content.toString() as string;
-    const containDsc = fileContent.includes(discontinuityString);
-    const containDscs = fileContent.includes(discontinuitySequenceString);
-    if (containDsc && !containDscs) {
-        const versionRex = /#EXT-X-VERSION:\d/g;
+    const h = fileContent.includes(discontinuityString);
+    if (h) {
         const regResult = versionRex.exec(fileContent);
         if (regResult !== null) {
             const versionString = regResult[0];
             const replaceStr = `${versionString}\n#EXT-X-DISCONTINUITY-SEQUENCE:${sequenceNum}`;
             const newFileContent = fileContent.replace(versionString, replaceStr);
-            await putStringToOss(fullPath, newFileContent);
+            const outName = `${dir}/${fileName}`;
+            const writeFile = createWriteStream(outName);
+            writeFile.write(newFileContent);
         }
     }
 }
 
-async function patchForM3U8(fileName: string, prefix: string): Promise<void> {
-    const ossResult = await ossClient.get(`${prefix}/${fileName}`);
-    const lines = (ossResult.content.toString() as string).split(/\r\n|\r|\n/g);
-    await patchLoopM3U8File(lines, fileName, prefix);
+function removeDir(dir: string): void {
+    const files = readdirSync(dir);
+    for (let i = 0; i < files.length; i++) {
+        const newPath = join(dir, files[i]);
+        const stat = statSync(newPath);
+        if (stat.isDirectory()) {
+            //如果是文件夹就递归下去
+            removeDir(newPath);
+        } else {
+            //删除文件
+            unlinkSync(newPath);
+        }
+    }
+    rmdirSync(dir); //如果文件夹是空的，就将自己删除掉
 }
 
-async function putStringToOss(fileName: string, content: string): Promise<boolean> {
-    const r = new Readable();
-    r._read = () => {};
-    r.push(content);
-    r.push(null);
-    const res = await ossClient.putStream(fileName, r);
-    return res.res.status === 200;
+async function patchForM3U8(fileName: string, prefix: string): Promise<void> {
+    const dirName = fileName.replace(/\.m3u8$/, "");
+    await patchLoopM3U8File(fileName, prefix, dirName);
+
+    const files = readdirSync(dirName, { withFileTypes: true })
+        .filter(dirent => dirent.isFile())
+        .map(dirent => dirent.name);
+
+    for await (const fileName of files) {
+        const fullFilePath = `${prefix}/${fileName}`;
+        await ossClient.delete(fullFilePath);
+        await ossClient.put(fullFilePath, join(dirName, fileName));
+    }
+
+    removeDir(dirName);
 }
 
 export { patchForM3U8 };
