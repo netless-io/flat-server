netless-io/flat-server

commit 6a91e5c644c6affda8db19e714c5971ce01f6ec7

refactor: remove region payload, add whiteboard.region config (#753)

src/v1/service/room/Room.ts
@@ -9,6 +9,7 @@ import { RoomStatus, RoomType } from "../../../model/room/Constants";
 import { whiteboardCreateRoom } from "../../utils/request/whiteboard/WhiteboardRequest";
 import { addHours, toDate } from "date-fns/fp";
 import { InsertResult } from "typeorm/query-builder/result/InsertResult";
+import { Whiteboard } from "../../../constants/Config";
 
 export class ServiceRoom {
     constructor(private readonly roomUUID: string, private readonly userUUID: string) {}
@@ -58,13 +59,13 @@ export class ServiceRoom {
         data: {
             title: string;
             type: RoomType;
-            region: Region;
             beginTime?: number | Date;
             endTime?: number | Date;
         },
         t?: EntityManager,
     ): Promise<InsertResult> {
-        const { title, type, region, endTime } = data;
+        const region = Whiteboard.region as Region;
+        const { title, type, endTime } = data;
         const beginTime = data.beginTime || Date.now();
 
         return await RoomDAO(t).insert({

config/defaults.yaml
@@ -182,6 +182,7 @@ whiteboard:
   app_id:
   access_key:
   secret_access_key:
+  region:
   convert_region:
 
 storage_service:

config/test.yaml
@@ -177,6 +177,7 @@ whiteboard:
   app_id: "test/flat-server"
   access_key: "test"
   secret_access_key: "test"
+  region: "cn-hz"
   convert_region: "cn-hz"
 
 storage_service:

src/constants/Config.ts
@@ -168,6 +168,7 @@ export const Whiteboard = {
     appId: config.whiteboard.app_id,
     accessKey: config.whiteboard.access_key,
     secretAccessKey: config.whiteboard.secret_access_key,
+    region: config.whiteboard.region,
     convertRegion: config.whiteboard.convert_region,
 };
 

src/utils/ParseConfig.ts
@@ -202,6 +202,7 @@ type Config = {
         app_id: string;
         access_key: string;
         secret_access_key: string;
+        region: "cn-hz" | "us-sv" | "sg" | "in-mum" | "gb-lon";
         convert_region: "cn-hz" | "us-sv" | "sg" | "in-mum" | "gb-lon";
     };
     storage_service: {

src/v1/controller/room/create/Ordinary.ts
@@ -25,7 +25,7 @@ export class CreateOrdinary extends AbstractController<RequestType, ResponseType
     public static readonly schema: FastifySchema<RequestType> = {
         body: {
             type: "object",
-            required: ["title", "type", "region"],
+            required: ["title", "type"],
             properties: {
                 title: {
                     type: "string",
@@ -48,6 +48,7 @@ export class CreateOrdinary extends AbstractController<RequestType, ResponseType
                 region: {
                     type: "string",
                     enum: [Region.CN_HZ, Region.US_SV, Region.SG, Region.IN_MUM, Region.GB_LON],
+                    nullable: true,
                 },
             },
         },
@@ -128,7 +129,7 @@ export interface RequestType {
         type: RoomType;
         beginTime?: number;
         endTime?: number;
-        region: Region;
+        region?: Region;
     };
 }
 

src/v1/controller/room/create/Periodic.ts
@@ -23,6 +23,7 @@ import { rtcQueue } from "../../../queue";
 import { aliGreenText } from "../../../utils/AliGreen";
 import { ControllerError } from "../../../../error/ControllerError";
 import { dataSource } from "../../../../thirdPartyService/TypeORMService";
+import { Whiteboard } from "../../../../constants/Config";
 
 @Controller<RequestType, ResponseType>({
     method: "post",
@@ -33,7 +34,7 @@ export class CreatePeriodic extends AbstractController<RequestType, ResponseType
     public static readonly schema: FastifySchema<RequestType> = {
         body: {
             type: "object",
-            required: ["title", "type", "beginTime", "endTime", "region", "periodic"],
+            required: ["title", "type", "beginTime", "endTime", "periodic"],
             properties: {
                 title: {
                     type: "string",
@@ -54,6 +55,7 @@ export class CreatePeriodic extends AbstractController<RequestType, ResponseType
                 region: {
                     type: "string",
                     enum: [Region.CN_HZ, Region.US_SV, Region.SG, Region.IN_MUM, Region.GB_LON],
+                    nullable: true,
                 },
                 periodic: {
                     type: "object",
@@ -105,7 +107,8 @@ export class CreatePeriodic extends AbstractController<RequestType, ResponseType
     private readonly periodicUUID = v4();
 
     public async execute(): Promise<Response<ResponseType>> {
-        const { title, type, beginTime, endTime, region, periodic } = this.body;
+        const region = Whiteboard.region as Region;
+        const { title, type, beginTime, endTime, periodic } = this.body;
         const userUUID = this.userUUID;
 
         if (await aliGreenText.textNonCompliant(title)) {
@@ -224,7 +227,7 @@ interface RequestType {
         type: RoomType;
         beginTime: number;
         endTime: number;
-        region: Region;
+        region?: Region;
         periodic: Periodic;
     };
 }

commit 6dbe31d88bf9422ca6eb4d91cbf72b4aa9b4b11a

fix typing issue introduced in 6a91e5c6

src/v1/service/room/Room.ts
@@ -59,6 +59,8 @@ export class ServiceRoom {
         data: {
             title: string;
             type: RoomType;
+            /** unused */
+            region?: Region;
             beginTime?: number | Date;
             endTime?: number | Date;
         },