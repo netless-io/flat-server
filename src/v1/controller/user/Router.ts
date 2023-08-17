f579617cd3f0d1ab709dee4e10ab17994a96cfa1
hyrious <hyrious@outlook.com>
2023-08-17 10:23:11 +0800

    fix(v1): register binding email routes

--- src/v1/controller/user/Router.ts
+++ src/v1/controller/user/Router.ts
@@ -1,6 +1,7 @@
 import { ControllerClass } from "../../../abstract/controller";
 import { Rename } from "./rename";
 import { SendMessage } from "./binding/platform/phone/SendMessage";
+import { SendMessage as SendEmail } from "./binding/platform/email/SendMessage";
 import { BindingPhone } from "./binding/platform/phone/Binding";
 import { DeleteAccountValidate } from "./deleteAccount/Validate";
 import { DeleteAccount } from "./deleteAccount";
@@ -14,6 +15,7 @@ import { RemoveBinding } from "./binding/Remove";
 import { BindingAgora } from "./binding/platform/agora/Binding";
 import { BindingGithub } from "./binding/platform/github/Binding";
 import { BindingGoogle } from "./binding/platform/google/Binding";
+import { BindingEmail } from "./binding/platform/email/Binding";
 
 export const userRouters: Readonly<Array<ControllerClass<any, any>>> = Object.freeze([
     Rename,
@@ -24,6 +26,8 @@ export const userRouters: Readonly<Array<ControllerClass<any, any>>> = Object.fr
     BindingGithub,
     BindingAgora,
     BindingGoogle,
+    BindingEmail,
+    SendEmail,
     SetAuthUUID,
     BindingProcess,
     BindingList,
