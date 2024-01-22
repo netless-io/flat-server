f6ad33bf36ee68963a52f4380ff07cbd8fd894e8
ooeyuna <ooe.yuna.rika@gmail.com>
2024-01-22 16:32:48 +0800

    fix join early (#808)
    * fix join early
    
    * fix import path
    
    ---------
    
    Co-authored-by: siyu <chensiyi@agora.io>

--- src/v1/controller/room/join/Ordinary.ts
+++ src/v1/controller/room/join/Ordinary.ts
@@ -9,7 +9,7 @@ import { Response } from "../../../../types/Server";
 import { RoomDAO, RoomUserDAO } from "../../../../dao";
 import { showGuide } from "./Utils";
 import { AGORA_SHARE_SCREEN_UID } from "../../../../constants/Agora";
-import { Server } from "constants/Config";
+import { Server } from "../../../../constants/Config";
 
 export const joinOrdinary = async (
     roomUUID: string,
@@ -44,7 +44,7 @@ export const joinOrdinary = async (
         };
     }
 
-    if (roomInfo.begin_time.getTime() - Date.now() <= Server.joinEarly * 60 * 1000) {
+    if (roomInfo.begin_time.getTime() - Date.now() > Server.joinEarly * 60 * 1000) {
         return {
             status: Status.Failed,
             code: ErrorCode.RoomNotBegin,
