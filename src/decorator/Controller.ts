5c0f1ce0cd0b2ec89a8231630f3e3b81a886e62a
siyu <chensiyi@agora.io>
2025-11-05 12:23:22 +0800

    add ipblock for sms and email

--- src/decorator/Controller.ts
+++ src/decorator/Controller.ts
@@ -4,12 +4,13 @@ import { ControllerStaticType } from "../abstract/controller";
 export const Controller = <RES, REQ>(
     params: ControllerParams,
 ): ((target: ControllerStaticType<RES, REQ>) => void) => {
-    const { path, method, auth, skipAutoHandle, enable } = params;
+    const { path, method, auth, ipblock, skipAutoHandle, enable } = params;
 
     return (target: ControllerStaticType<RES, REQ>): void => {
         Reflect.defineMetadata(RouterMetadata.PATH, path, target);
         Reflect.defineMetadata(RouterMetadata.METHOD, method, target);
         Reflect.defineMetadata(RouterMetadata.AUTH, auth, target);
+        Reflect.defineMetadata(RouterMetadata.IPBLOCK, ipblock || false, target);
         Reflect.defineMetadata(RouterMetadata.SKIP_AUTO_HANDLE, skipAutoHandle || false, target);
         Reflect.defineMetadata(RouterMetadata.ENABLE, enable ?? true, target);
     };
@@ -19,6 +20,7 @@ export type ControllerParams = {
     path: string | string[];
     method: "get" | "post";
     auth: boolean;
+    ipblock?: boolean;
     skipAutoHandle?: boolean;
     enable?: boolean;
 };
